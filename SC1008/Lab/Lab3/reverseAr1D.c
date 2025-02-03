08ef3a41f7500b54f396483ba546732d3f5eb498
Sb93 <66685463+Shadowblazer93@users.noreply.github.com>
2025-02-03 12:17:57 +0800

    SC1008 LAB 3

---
+++ SC1008/Lab/Lab3/reverseAr1D.c
@@ -0,0 +1,22 @@
+#include <stdio.h> 
+void reverseAr1D(int ar[], int size); 
+
+int main() { 
+    int ar[10]; 
+    int size, i; 
+    
+    printf("Enter array size: \n"); 
+    scanf("%d", &size); 
+    printf("Enter %d data: \n", size); 
+    for (i=0; i <= size-1; i++) scanf("%d", &ar[i]); 
+    reverseAr1D(ar, size); 
+    printf("reverseAr1D(): "); 
+    if (size > 0) { 
+        for (i=0; i<size; i++) printf("%d ", ar[i]); 
+    } 
+} 
+void reverseAr1D(int ar[], int size) {
+    int arcopy[size];
+    for (int i=0; i<size; i++) arcopy[i] = ar[size-i-1];
+    for (int i=0; i<size; i++) ar[i] = arcopy[i];
+}
